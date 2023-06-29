ec3724b69610d38c7bbfb2dcc2640a8a0f1932f6
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2023-06-29 09:31:08 +0200

    update paths

--- env/nvidia-gpu.sh
+++ env/nvidia-gpu.sh
@@ -2,5 +2,6 @@
 source /applis/site/guix-start.sh
 guix install cmake
 source /applis/environments/cuda_env.sh bigfoot 11.2
-export IDEFIX_DIR=~/src/idefix
-export NVIDIA_FLAGS="-DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_AMPERE80=ON"
+export IDEFIX_DIR=/bettik/PROJECTS/pr-idefix-23/COMMON/src/idefix
+export KOKKOS_TOOLS_DIR=/bettik/PROJECTS/pr-idefix-23/COMMON/bin/nvidia/kokkos-tools
+export NVIDIA_FLAGS="-DKokkos_ENABLE_CUDA=ON -DKokkos_ARCH_VOLTA70=ON"
