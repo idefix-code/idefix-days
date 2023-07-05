f915ea4d169e55556127733b213c50519f0626d0
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2023-07-05 14:10:11 +0200

    Update nvidia-gpu.sh

--- env/nvidia-gpu.sh
+++ env/nvidia-gpu.sh
@@ -1,5 +1,5 @@
 #install guix environement for cuda
-source /applis/site/guix-start.sh
+# source /applis/site/guix-start.sh
 export PATH=$PATH:/gnu/store/1w01prf09gi6d9i976n1r95lj70yq4sd-cmake-3.21.4/bin
 source /applis/environments/cuda_env.sh bigfoot 11.2
 export IDEFIX_DIR=/bettik/PROJECTS/pr-idefix-23/COMMON/src/idefix
